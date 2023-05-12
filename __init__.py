fce3287d0d11134011a6bc0f1c9e894c5a0ae4a9
Franco Caspe <francocaspe@gmail.com>
2023-05-12 20:11:50 +0100

    Updated structure

--- __init__.py
+++
@@ -1,2 +0,0 @@
-from .synth import dx7_synth,midi_note
-__all__ =['dx7_synth','midi_note']
---
+++ pydx7/__init__.py
@@ -0,0 +1,3 @@
+from .synth import dx7_synth,midi_note
+from .dx7tools import render_envelopes, load_patch_from_bulk
+__all__ =['dx7_synth','midi_note','render_envelopes','load_patch_from_bulk']
