87715df5ecb301c32634af3d9e5fe29596059c24
Franco Caspe <francocaspe@gmail.com>
2023-10-01 17:59:37 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='pydx7', version='0.1', packages=find_packages())
+setup(name='pydx7', version='0.1', packages=find_packages(),
+      install_requires=['einops>=0.7.0',
+                        'numba>=0.57.0',
+                        'numpy>=1.23.5',
+                        'setuptools>=65.6.3'])
